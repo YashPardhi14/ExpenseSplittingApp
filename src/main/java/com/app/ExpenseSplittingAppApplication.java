5ee5532928de19a4ea0e2916bc4a528827bac3d3
YashPardhi14 <yapardhi@gmail.com>
2024-01-30 16:05:09 +0530

    First Commit

---
+++ src/main/java/com/app/ExpenseSplittingAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSplittingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSplittingAppApplication.class, args);
+	}
+
+}
